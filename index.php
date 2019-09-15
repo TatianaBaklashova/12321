3b6580c7bca183eef6639709cd2d97bd35e02bdb
TatianaBaklashova <55283577+TatianaBaklashova@users.noreply.github.com>
2019-09-15 17:26:49 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,98 @@
 <?php
+    // объявляем класс с именем Rectangle
+    class Rectangle
+    {
+        // свойство "ширина"
+        private $width;
+        // свойство "высота"
+        private $height;
 
-function fib($n)
-{
-    $result = [1, 1];
+        // конструктор класса
+        public function __construct($x, $y)
+        {
+            $this->width = $x;
+            $this->height = $y;
+        }
 
-    for($i = 2; $i <= $n; $i++ ) {
-        $result[] = $result[$i-1] + $result[$i-2];
+        // метод "посчитать площадь"
+        public function Square()
+        {
+            return $this->width * $this->height;
+        }
     }
-    return $result;
-}
 
-print_r(fib(64));
+ // объявляем класс с именем Circle
+   class Circle
+    {
+        // свойство "радиус"
+        private $radius;
+
+        // конструктор класса
+        public function __construct($r)
+        {
+            $this->radius = $r;
+        }
+
+        // метод "посчитать площадь"
+        public function Square()
+        {
+            return M_PI*pow($this->radius,2);
+        }
+    }
+
+ // объявляем класс с именем Triangle
+   class Triangle
+    {
+        // свойство "основание"
+        private $base;
+        //свойство "высота"
+        private $height;
+        // конструктор класса
+        public function __construct($x,$y)
+        {
+            $this->base = $x;
+            $this->height = $y;
+        }
+
+        // метод "посчитать площадь"
+        public function Square()
+        {
+            return 0.5*$this->base*$this->height;
+        }
+    }
+    $rects = array();
+    // заполняем массив экземплярами
+    $rects['1'] = new Rectangle(rand(10,50), rand(10,50));
+    $rects['2'] = new Circle(rand(10,50));
+    $rects['3'] = new Triangle(rand(10,50), rand(10,50));
+    //массив для площадей фигрур
+    $area= array();
+    // выводим площадь каждого экземпляра
+    foreach($rects as $key => $rect)
+    {
+        if($key==1)
+        {
+        echo 'Площадь прямоугольника: ' . $rect->Square() .
+                "<br />\n";
+                $area[]=$rect->Square();
+        }
+        if($key==2)
+            {  
+        echo 'Площадь круга: ' . $rect->Square() .
+                "<br />\n";
+                $area[]=$rect->Square();
+            }
+        if($key==3)
+            {  
+        echo 'Площадь треугольника: ' . $rect->Square() .
+                "<br />\n";
+                $area[]=$rect->Square();
+            }
+    }
+    //сортировка массива с площадями фигур по убыванию
+    arsort($area);
+    //вывод отсортированного массива
+    print_r($area);
+
+
 ?>
